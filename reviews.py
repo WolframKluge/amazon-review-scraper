adbbaf75167fd6936ca6c5f74c7d31c9a9cfa5df
Wolfram Kluge <wolfram@klugelabs.co>
2021-01-09 20:11:21 +0100

    added next page scraping currently with static limit.

--- reviews.py
+++ reviews.py
@@ -1,14 +1,14 @@
 from selectorlib import Extractor
-import requests 
-import json 
-from time import sleep
+import requests
 import csv
 from dateutil import parser as dateparser
 
 # Create an Extractor by reading from the YAML file
 e = Extractor.from_yaml_file('selectors.yml')
+PAGE_NUMBER = "&pageNumber="
 
-def scrape(url):    
+
+def scrape(base_url, limit):
     headers = {
         'authority': 'www.amazon.com',
         'pragma': 'no-cache',
@@ -23,39 +23,70 @@ def scrape(url):
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
 
+    writer = csv.DictWriter(outfile,
+                            fieldnames=["title", "content", "date", "variant", "images", "verified", "author", "rating",
+                                        "product", "url"], quoting=csv.QUOTE_ALL)
+    writer.writeheader()
+
+    page_index = 1
+    review_count = 0
+    review_data = []
+    while review_count < limit:
+        next_url = base_url + PAGE_NUMBER + str(page_index)
+        page_index += 1
+
+        data_from_extractor = download(next_url,headers)
+
+        data_set = load_n_transform(data_from_extractor)
+
+        review_data.extend(data_set)
+        review_count = len(review_data)
+
+        writer.writerows(data_set)
+    return 1
+
+
+
+def load_n_transform(data):
+    data_set = []
+    if data:
+        for r in data["reviews"]:
+            r["product"] = data["product_title"]
+            r['url'] = url
+            if 'verified' in r:
+                if 'Verified Purchase' in r['verified']:
+                    r['verified'] = 'Yes'
+                else:
+                    r['verified'] = 'Yes'
+            r['rating'] = r['rating'].split(' out of')[0]
+            date_posted = r['date'].split('on ')[-1]
+            if r['images']:
+                r['images'] = "\n".join(r['images'])
+            r['date'] = dateparser.parse(date_posted).strftime('%d %b %Y')
+            data_set.append(r)
+        return data_set
+
+def download(url, headers):
     # Download the page using requests
-    print("Downloading %s"%url)
+    print("Downloading %s" %url)
     r = requests.get(url, headers=headers)
     # Simple check to check if page was blocked (Usually 503)
     if r.status_code > 500:
         if "To discuss automated access to Amazon data please contact" in r.text:
-            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+            print("Page %s was blocked by Amazon. Please try using better proxies\n" % url)
         else:
-            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
-        return None
-    # Pass the HTML of the page and create 
+            print("Page %s must have been blocked by Amazon as the status code was %d" % (url, r.status_code))
+    # Pass the HTML of the page and create
+
     return e.extract(r.text)
 
+
+def print_data(data):
+    for row in data:
+        print(row)
+
 # product_data = []
-with open("urls.txt",'r') as urllist, open('data.csv','w') as outfile:
-    writer = csv.DictWriter(outfile, fieldnames=["title","content","date","variant","images","verified","author","rating","product","url"],quoting=csv.QUOTE_ALL)
-    writer.writeheader()
-    for url in urllist.readlines():
-        data = scrape(url) 
-        if data:
-            for r in data['reviews']:
-                r["product"] = data["product_title"]
-                r['url'] = url
-                if 'verified' in r:
-                    if 'Verified Purchase' in r['verified']:
-                        r['verified'] = 'Yes'
-                    else:
-                        r['verified'] = 'Yes'
-                r['rating'] = r['rating'].split(' out of')[0]
-                date_posted = r['date'].split('on ')[-1]
-                if r['images']:
-                    r['images'] = "\n".join(r['images'])
-                r['date'] = dateparser.parse(date_posted).strftime('%d %b %Y')
-                writer.writerow(r)
-            # sleep(5)
-    
+with open("urls.txt", 'r') as url_list, open('data.csv', 'w') as outfile:
+    for url in url_list.readlines():
+        data = scrape(url, limit=20000)
+
